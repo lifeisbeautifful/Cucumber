7257b8ee9220aec00f30aa45d2139f9dc3bc6e86
unknown <oksankagnyp@gmail.com>
2024-08-13 10:36:51 +0300

    Added cucumber html report with screenshots

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,17 +1,33 @@
-import { BeforeAll, AfterAll, Before, After } from "@cucumber/cucumber";
-import { chromium, Browser, Page } from "@playwright/test";
+import { BeforeAll, AfterAll, Before, After, Status } from "@cucumber/cucumber";
+import { chromium, Browser, Page, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser;
-let page: Page;
+let context: BrowserContext;
+
+BeforeAll(async function() {
+    browser = await chromium.launch({ headless: false });
+})
 
 Before(async function() {
-    browser = await chromium.launch({ headless: false });
-    page = await browser.newPage();
+    context = await browser.newContext();
+    const page = await browser.newPage();
     pageFixture.page = page;
 })
 
-After(async function() {
+After(async function({ pickle, result }) {
+    console.log(result?.status);
+    
+    if(result?.status == Status.FAILED){
+        //screenshot
+    const img = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${ pickle.name }.png`, type: "png" });
+    await this.attach(img, "image/png");
+    }
+    
     await pageFixture.page.close();
+    await context.close();
+})
+
+AfterAll(async function() {
     await browser.close();
 })
