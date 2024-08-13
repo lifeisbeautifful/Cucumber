036a703b60a69abefb0fc605568b0adac54b3d9f
unknown <oksankagnyp@gmail.com>
2024-08-13 15:53:26 +0300

    Added winston logger

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,8 +1,10 @@
 import { BeforeAll, AfterAll, Before, After, Status } from "@cucumber/cucumber";
-import { chromium, Browser, Page, BrowserContext } from "@playwright/test";
+import { Browser, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 import { invokeBrowser } from "../helper/browsers/browserManager";
 import { getEnv } from "../helper/env/env";
+import { createLogger } from "winston";
+import { options } from "../helper/util/logger";
 
 let browser: Browser;
 let context: BrowserContext;
@@ -12,10 +14,12 @@ BeforeAll(async function() {
     browser = await invokeBrowser();
 })
 
-Before(async function() {
+Before(async function({ pickle }) {
+    const scenarioName = pickle.name + pickle.id;
     context = await browser.newContext();
     const page = await browser.newPage();
     pageFixture.page = page;
+    pageFixture.logger = createLogger(options(scenarioName));
 })
 
 After(async function({ pickle, result }) {
@@ -33,4 +37,5 @@ After(async function({ pickle, result }) {
 
 AfterAll(async function() {
     await browser.close();
+    await pageFixture.logger.close();
 })
--- src/test/steps/addToCartSteps.ts
+++ src/test/steps/addToCartSteps.ts
@@ -8,6 +8,7 @@ setDefaultTimeout(60 * 1000 * 2);
 Given('User is navigated to home page', { timeout: 10000},  async function () {
     await pageFixture.page.goto(process.env.BASEURL);
     await expect(pageFixture.page).toHaveURL("https://allo.ua/");
+    pageFixture.logger.info("Navigation to home page");
 });
 
 When('Items catalog is opened', async function () {
@@ -19,7 +20,9 @@ When('Items catalog is opened', async function () {
     let catalog = await pageFixture.page.locator("#js-menu-wrapper");
     let categoryItem = await catalog.getByText(item, {exact:true});
     await categoryItem.hover();
+    pageFixture.logger.info("select " + item + " item");
     await this.catalog.getByRole("link", {name:`${subItem }`, exact:true}).click();
+    pageFixture.logger.info("select " + subItem + " subitem");
   });
 
 Then('User is navigated to {string}', async function (url) {
